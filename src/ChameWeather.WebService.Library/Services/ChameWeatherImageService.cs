a2b6e9ac9fda4622749ca8d13b662f3680fd03c4
jar.bob@gmail.com <jar.bob@gmail.com>
2015-05-15 21:50:01 +0800

    Init source code from TFS

---
+++ src/ChameWeather.WebService.Library/Services/ChameWeatherImageService.cs
@@ -0,0 +1,50 @@
+﻿using Chame.WebService.Helper.Models;
+using Chame.WebService.Helper.Services;
+using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChameWeather.WebService.Library.Services
+{
+    public class ChameWeatherImageService : IChameService
+    {
+        private readonly ILog logger;
+
+        public ChameWeatherImageService(ILog logger)
+        {
+            this.logger = logger;
+        }
+
+        public void Dispose()
+        {
+            // Not implement, to clean up instance
+        }
+
+        #region Properties
+
+        public string ServiceName
+        {
+            get { return "ChameWeather.Service"; }
+        }
+
+        #endregion
+
+        public Task<IChameResponse> ProcessImage(string request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IChameResponse> GetCache()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IChameResponse> CleanCache(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
